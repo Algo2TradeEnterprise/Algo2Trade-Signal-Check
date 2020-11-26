1a602f16f713e12843f00c22d57083de616d493f
Indibar <indibar@algo2trade.com>
2020-11-26 20:07:41 +0530

    After fix

--- Algo2Trade Signal Check/StrategyRules/DataTester.vb
+++ Algo2Trade Signal Check/StrategyRules/DataTester.vb
@@ -8,124 +8,33 @@ Public Class DataTester
     Public Overrides Async Function RunAsync(ByVal startDate As Date, ByVal endDate As Date) As Task(Of DataTable)
         Await Task.Delay(0).ConfigureAwait(False)
         Dim ret As New DataTable
-        ret.Columns.Add("SnapshotDateTime")
-        ret.Columns.Add("TradingSymbol")
-        ret.Columns.Add("Open")
-        ret.Columns.Add("Low")
-        ret.Columns.Add("High")
+        ret.Columns.Add("Trading Symbol")
+        ret.Columns.Add("Date")
         ret.Columns.Add("Close")
-        ret.Columns.Add("Volume")
 
         Dim stockData As StockSelection = New StockSelection(_canceller, _category, _cmn, _fileName)
         AddHandler stockData.Heartbeat, AddressOf OnHeartbeat
         AddHandler stockData.WaitingFor, AddressOf OnWaitingFor
         AddHandler stockData.DocumentRetryStatus, AddressOf OnDocumentRetryStatus
         AddHandler stockData.DocumentDownloadComplete, AddressOf OnDocumentDownloadComplete
-        Dim chkDate As Date = startDate
-        While chkDate <= endDate
-            _canceller.Token.ThrowIfCancellationRequested()
-            Dim stockList As List(Of String) = Nothing
-            If _instrumentName Is Nothing OrElse _instrumentName = "" Then
-                stockList = Await stockData.GetStockList(chkDate).ConfigureAwait(False)
-            Else
-                stockList = New List(Of String)
-                stockList.Add(_instrumentName)
-            End If
-            _canceller.Token.ThrowIfCancellationRequested()
-            If stockList IsNot Nothing AndAlso stockList.Count > 0 Then
-                For Each stock In stockList
-                    _canceller.Token.ThrowIfCancellationRequested()
-                    Dim xStockPayload As Dictionary(Of Date, Payload) = Nothing
-                    Dim nifty50Payload As Dictionary(Of Date, Payload) = Nothing
-                    Select Case _category
-                        Case Common.DataBaseTable.Intraday_Cash, Common.DataBaseTable.Intraday_Commodity, Common.DataBaseTable.Intraday_Currency, Common.DataBaseTable.Intraday_Futures
-                            xStockPayload = _cmn.GetRawPayload(_category, stock, chkDate.AddDays(-8), chkDate)
-                            nifty50Payload = _cmn.GetRawPayload(Common.DataBaseTable.Intraday_Cash, "NIFTY 50", chkDate.AddDays(-8), chkDate)
-                        Case Common.DataBaseTable.EOD_Cash, Common.DataBaseTable.EOD_Commodity, Common.DataBaseTable.EOD_Currency, Common.DataBaseTable.EOD_Futures, Common.DataBaseTable.EOD_POSITIONAL
-                            xStockPayload = _cmn.GetRawPayload(_category, stock, chkDate.AddDays(-200), chkDate)
-                            nifty50Payload = _cmn.GetRawPayload(Common.DataBaseTable.EOD_Cash, "NIFTY 50", chkDate.AddDays(-200), chkDate)
-                        Case Common.DataBaseTable.Intraday_Futures_Options
-                            xStockPayload = _cmn.GetRawPayloadForSpecificTradingSymbol(_category, stock, chkDate.AddDays(-8), chkDate)
-                            nifty50Payload = _cmn.GetRawPayload(Common.DataBaseTable.Intraday_Cash, "NIFTY 50", chkDate.AddDays(-8), chkDate)
-                        Case Common.DataBaseTable.EOD_Futures_Options
-                            xStockPayload = _cmn.GetRawPayloadForSpecificTradingSymbol(_category, stock, chkDate.AddDays(-200), chkDate)
-                            nifty50Payload = _cmn.GetRawPayload(Common.DataBaseTable.EOD_Cash, "NIFTY 50", chkDate.AddDays(-200), chkDate)
-                    End Select
-                    _canceller.Token.ThrowIfCancellationRequested()
-                    If xStockPayload IsNot Nothing AndAlso xStockPayload.Count > 0 AndAlso
-                        nifty50Payload IsNot Nothing AndAlso nifty50Payload.Count > 0 Then
-                        Dim dateList As List(Of Date) = nifty50Payload.Keys.ToList
-                        Dim preDate As Date = Date.MinValue
-                        For Each runningDate In dateList
-                            _canceller.Token.ThrowIfCancellationRequested()
-                            If Not xStockPayload.ContainsKey(runningDate) Then
-                                If preDate <> Date.MinValue AndAlso xStockPayload.ContainsKey(preDate) Then
-                                    xStockPayload.Add(runningDate, xStockPayload(preDate))
-                                End If
-                            End If
-                            preDate = runningDate
-                        Next
-
-                        Dim stockPayload As Dictionary(Of Date, Payload) = Nothing
-                        For Each runningPayload In xStockPayload.OrderBy(Function(x)
-                                                                             Return x.Key
-                                                                         End Function)
-                            If stockPayload Is Nothing Then stockPayload = New Dictionary(Of Date, Payload)
-                            stockPayload.Add(runningPayload.Key, runningPayload.Value)
-                        Next
+        _canceller.Token.ThrowIfCancellationRequested()
 
 
-                        Dim XMinutePayload As Dictionary(Of Date, Payload) = Nothing
-                        Dim exchangeStartTime As Date = Date.MinValue
-                        Select Case _category
-                            Case Common.DataBaseTable.EOD_Cash, Common.DataBaseTable.EOD_Futures, Common.DataBaseTable.EOD_POSITIONAL, Common.DataBaseTable.Intraday_Cash, Common.DataBaseTable.Intraday_Futures
-                                exchangeStartTime = New Date(chkDate.Year, chkDate.Month, chkDate.Day, 9, 15, 0)
-                            Case Common.DataBaseTable.EOD_Commodity, Common.DataBaseTable.EOD_Currency, Common.DataBaseTable.Intraday_Commodity, Common.DataBaseTable.Intraday_Currency
-                                exchangeStartTime = New Date(chkDate.Year, chkDate.Month, chkDate.Day, 9, 0, 0)
-                        End Select
-                        If _timeFrame > 1 Then
-                            XMinutePayload = Common.ConvertPayloadsToXMinutes(stockPayload, _timeFrame, exchangeStartTime)
-                        Else
-                            XMinutePayload = stockPayload
-                        End If
-                        _canceller.Token.ThrowIfCancellationRequested()
-                        Dim inputPayload As Dictionary(Of Date, Payload) = Nothing
-                        If _useHA Then
-                            Indicator.HeikenAshi.ConvertToHeikenAshi(XMinutePayload, inputPayload)
-                        Else
-                            inputPayload = XMinutePayload
-                        End If
-                        _canceller.Token.ThrowIfCancellationRequested()
-                        Dim currentDayPayload As Dictionary(Of Date, Payload) = Nothing
-                        For Each runningPayload In inputPayload.Keys
-                            _canceller.Token.ThrowIfCancellationRequested()
-                            If runningPayload.Date = chkDate.Date Then
-                                If currentDayPayload Is Nothing Then currentDayPayload = New Dictionary(Of Date, Payload)
-                                currentDayPayload.Add(runningPayload, inputPayload(runningPayload))
-                            End If
-                        Next
+        Dim eodPayload As Dictionary(Of Date, Payload) = _cmn.GetRawPayloadForSpecificTradingSymbol(_category, _instrumentName, startDate, endDate)
+        If eodPayload IsNot Nothing AndAlso eodPayload.Count > 0 Then
+            Dim weeklyPayload As Dictionary(Of Date, Payload) = Common.ConvertDayPayloadsToWeek(eodPayload)
+            If weeklyPayload IsNot Nothing AndAlso weeklyPayload.Count > 0 Then
+                For Each runningPayload In weeklyPayload.Values
+                    Dim row As DataRow = ret.NewRow
+                    row("Trading Symbol") = runningPayload.TradingSymbol
+                    row("Date") = runningPayload.PayloadDate.ToString("dd-MMM-yyyy")
+                    row("Close") = runningPayload.Close
 
-                        'Main Logic
-                        If currentDayPayload IsNot Nothing AndAlso currentDayPayload.Count > 0 Then
-                            For Each runningPayload In currentDayPayload.Keys
-                                _canceller.Token.ThrowIfCancellationRequested()
-                                Dim row As DataRow = ret.NewRow
-                                row("SnapshotDateTime") = runningPayload.ToString("dd-MM-yyyy HH:mm:ss")
-                                row("TradingSymbol") = inputPayload(runningPayload).TradingSymbol
-                                row("Open") = inputPayload(runningPayload).Open
-                                row("Low") = inputPayload(runningPayload).Low
-                                row("High") = inputPayload(runningPayload).High
-                                row("Close") = inputPayload(runningPayload).Close
-                                row("Volume") = inputPayload(runningPayload).Volume
-
-                                ret.Rows.Add(row)
-                            Next
-                        End If
-                    End If
+                    ret.Rows.Add(row)
                 Next
             End If
-            chkDate = chkDate.AddDays(1)
-        End While
+        End If
         Return ret
     End Function
+
 End Class
